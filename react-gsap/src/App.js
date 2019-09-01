958e8f7ab5ff225fde3eff37ccc2431da79e4512
haluskua <haluskua.one@gmail.com>
2019-09-02 08:48:37 +1200

    3dots with hover state exchange

--- react-gsap/src/App.js
+++ react-gsap/src/App.js
@@ -1,57 +1,73 @@
 import React from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import { TweenMax, Power3 } from "gsap";
 
 function App() {
-  let logoItem = useRef(null);
-  let textItem = useRef(null);
+  //create ref
+  let app = useRef(null);
+  let circle = useRef(null);
+  let circleRed = useRef(null);
+  let circleBlue = null;
 
-  console.log(logoItem);
+  const [state, setstate] = useState(false);
 
-  useEffect(() => {
-    console.log(logoItem);
-    // logoItem.style.display = "none";
-    TweenMax.to(logoItem, 2, {
-      opacity: 1,
-      y: -50,
+  const handleExpand = () => {
+    TweenMax.to(circleRed, 0.8, {
+      width: 200,
+      height: 200,
       ease: Power3.easeOut
     });
-    TweenMax.to(textItem, 4, {
-      opacity: 1,
-      y: -50,
-      ease: Power3.easeOut,
-      delay: 0.2
+    setstate(true);
+  };
+  const handleShrink = () => {
+    TweenMax.to(circleRed, 0.8, {
+      width: 75,
+      height: 75,
+      ease: Power3.easeOut
     });
+    setstate(false);
+  };
+  
+  useEffect(() => {
+    TweenMax.to(app, 0, { css: { visibility: "visible" } });
+    // TweenMax.from(circle, 0.8, { opacity: 0, x: 40, ease: Power3.easeOut });
+    // TweenMax.from(circleRed, 0.8, {
+    //   opacity: 0,
+    //   x: 40,
+    //   ease: Power3.easeOut,
+    //   delay: 0.2
+    // });
+    // TweenMax.from(circleBlue, 0.8, {
+    //   opacity: 0,
+    //   x: 40,
+    //   ease: Power3.easeOut,
+    //   delay: 0.4
+    // });
+    TweenMax.staggerFrom(
+      [circle, circleRed, circleBlue],
+      0.8,
+      { opacity: 0, x: 40, ease: Power3.easeOut },
+      0.2
+    );
   }, []);
-
   return (
-    <div className="App">
+    <div ref={el => (app = el)} className="App">
       <header className="App-header">
-        <img
-          ref={el => {
-            logoItem = el;
-          }}
-          src={logo}
-          className="App-logo"
-          alt="logo"
-        />
-        <p
-          ref={el => {
-            textItem = el;
-          }}
-        >
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="circle-container">
+          <div ref={el => (circle = el)} className="circle"></div>
+          <div
+            onMouseOver={state !== true ? handleExpand : handleShrink}
+            ref={el => (circleRed = el)}
+            className="circle red"
+          ></div>
+          <div
+            onMouseOver={state !== true ? handleExpand : handleShrink}
+            ref={el => (circleBlue = el)}
+            className="circle blue"
+          ></div>
+        </div>
       </header>
     </div>
   );
